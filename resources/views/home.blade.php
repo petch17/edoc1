5f8452f0ca809ea502fda3dcbb46877c2296a118
NAME <psp_phet@hotmail.com>
2019-08-08 09:47:08 +0700

    แก้ไขเล็กน้อย

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,62 +2,6 @@
 
 @section('content')
 
-    <center> 
-        <div class="page-header-title">
-            <h1> HEAD </h1>           
-        </div>
-        <div class="page-body">
-            <a href="#" style="text-decoration:none"> หัวข้อ1 &nbsp; </a>
-            <a href="#" style="text-decoration:none"> หัวข้อ2 &nbsp; </a>
-            <a href="#" style="text-decoration:none"> หัวข้อ3 &nbsp; </a>
-            <a href="#" style="text-decoration:none"> หัวข้อ4 &nbsp; </a>
-        </div>
-    </center>
-    <form action="#" method="POST">
-
-        Short Description: <br>
-        <textarea name="editor1" rows="10" cols="50" ></textarea> <br>
-
-        <script type="text/javascript">
-
-            CKEDITOR.replace('editor1', {
-
-                height: "200px", //กำหนดความสูง  
-
-                width: "720px", //กำหนดความกว้าง  
-
-                toolbar: //กลุ่มเครื่องมือ ตัดออก เพิ่มเข้าได้  
-
-                        [
-                            [
-                                'Bold', 'Italic', 'Underline', '-',
-                                'Subscript', 'Superscript', '-',
-
-                                'NumberedList', 'BulletedList', '-',
-                                'Link', 'Unlink', '-',
-                                'Cut', 'Copy', 'Paste', '-',
-                                'Undo', 'Redo'
-                            ],
-
-                            [
-                                'Outdent', 'Indent', '-',
-                                'JustifyCenter', 'JustifyRight', 'JustifyBlock'
-                            ],
-
-                            '/',
-
-                            [
-                                'Image', 'Table', 'HorizontalRule', '-',
-                                'Font', 'FontSize', 'TextColor', 'BGColor'
-                            ]
-                        ]
-
-            }
-            );
-
-        </script> <br>
-
-        <input type="submit" name="submit" value="ยืนยัน">
-    </form>
+    
 
 @endsection
