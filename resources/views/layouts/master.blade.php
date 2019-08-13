2943054a01b356616a211724df7e8a7f2046c997
NAME <psp_phet@hotmail.com>
2019-08-13 14:19:01 +0700

    เอารูปแจ้งเตือนหน้าหลักออก

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -90,35 +90,6 @@
 
                                 </ul>
                                 <ul class="nav-right">
-
-                                    <li class="header-notification">
-                                        <a href="#!">
-                                            <i class="ti-bell"></i>
-                                            <span class="badge">1</span>
-                                        </a>
-                                        <ul class="show-notification">
-                                            <li>
-                                                <h6>Notifications</h6>
-                                                <label class="label label-danger">New</label>
-                                            </li>
-                                            <li>
-                                                <div class="media">
-                                                    <img class="d-flex align-self-center" src="assets/images/user.png" alt="Generic placeholder image">
-                                                    <div class="media-body">
-                                                        <h5 class="notification-user">John Doe</h5>
-                                                        <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
-                                                        <span class="notification-time">30 minutes ago</span>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="header-notification">
-                                        <a href="#!" class="displayChatbox">
-                                            <i class="ti-comments"></i>
-                                            <span class="badge">2</span>
-                                        </a>
-                                    </li>
                                     <li class="user-profile header-notification">
                                         <a href="#!">
                                             <img src="assets/images/user.png" alt="User-Profile-Image">
@@ -238,7 +209,7 @@
 
                                         </div>
                                         <div class="page-body">
-                                        
+
                                             @yield('content')
 
                                         </div>
